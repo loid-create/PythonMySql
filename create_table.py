d769735aad56dbbf4a7c1fe14e58583f1ff8c5db
loid-create <idla3007@gmail.com>
2021-04-15 14:32:03 +0700

    create table

---
+++ create_table.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    passwd = "",
+    database = "shop"
+)
+
+cursor = db.cursor()
+sql = """CREATE TABLE customers (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    address VARCHAR(255)
+)"""
+cursor.execute(sql)
+
+print("Table customers berhasil dibuat !")

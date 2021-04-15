9b161a928f46e4cdde070486a75fa393ea00d02e
loid-create <idla3007@gmail.com>
2021-04-15 14:37:17 +0700

    insert data

---
+++ insert_one.py
@@ -0,0 +1,16 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    passwd = "",
+    database = "shop"
+)
+
+cursor = db.cursor()
+sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+val = ("Dian", "Mataram")
+cursor.execute(sql, val)
+
+db.commit()
+print("{} data ditambahkan".format(cursor.rowcount))
